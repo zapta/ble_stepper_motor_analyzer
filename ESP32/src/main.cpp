972aa3d6952d5c7c70e3a2cf964e4a0b27248dee
Zapta <zapta@users.noreply.github.com>
2022-09-02 06:53:13 -0700

    Experimenting.

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -1,5 +1,24 @@
+// #include <string.h>
 
-void my_main() {}
+#include "esp_event.h"
+// #include "esp_log.h"
+#include "esp_system.h"
+// #include "esp_wifi.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+// #include "lwip/err.h"
+// #include "lwip/sys.h"
+// #include "nvs_flash.h"
+
+#define TAG "xyz"
+
+void my_main() {
+  for (;;) {
+    sys_delay_ms(500);
+    // ESP_LOGI(TAG, "uuhello world");
+    printf("printf\n");
+  }
+}
 
 // The runtime environment expects a "C" main.
 extern "C" {
