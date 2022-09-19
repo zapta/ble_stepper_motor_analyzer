30bf3246d9274e0464422b6309971e036f5ae525
Zapta <zapta@users.noreply.github.com>
2022-09-18 22:06:15 -0700

    Added skeleton ble files.

---
+++ platformio/src/misc/ble_service.cpp
@@ -0,0 +1,29 @@
+#include "misc/ble_service.h"
+
+// https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/bluetooth/esp_gatts.html
+//
+// https://github.com/espressif/esp-idf/tree/master/examples/bluetooth/bluedroid/ble/gatt_server_service_table
+
+// https://github.com/espressif/esp-idf/blob/master/examples/bluetooth/bluedroid/ble/gatt_server_service_table/tutorial/Gatt_Server_Service_Table_Example_Walkthrough.md
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "bt.h"
+#include "bta_api.h"
+
+#include "esp_gap_ble_api.h"
+#include "esp_gatts_api.h"
+#include "esp_bt_defs.h"
+#include "esp_bt_main.h"
+#include "esp_bt_main.h"
+#include “gatts_table_creat_demo.h"
+
+
+namespace ble {
+
+    
+}
