34a7aea26060ddcf167072f1a7979661520114d5
Zapta <zapta@users.noreply.github.com>
2023-01-03 21:50:45 -0800

    Cleanup.

--- platformio/src/acquisition/adc_task.cpp
+++ platformio/src/acquisition/adc_task.cpp
@@ -9,35 +9,34 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "io/io.h"
-#include "sdkconfig.h"
 #include "misc/elapsed.h"
+#include "sdkconfig.h"
 
-namespace adc_task { 
+namespace adc_task {
 
 // Should be equivalent to sizeof(*adc_digi_output_data_t)
-static constexpr uint32_t _BYTES_PER_VALUE = 2;
+constexpr uint32_t BYTES_PER_VALUE = 2;
 
 // Number of pairs of samples per a read packet.
-static constexpr uint32_t _VALUE_PAIRS_PER_BUFFER = 200;
+constexpr uint32_t VALUE_PAIRS_PER_BUFFER = 200;
 
-static constexpr uint32_t _VALUES_PER_BUFFER = 2 * _VALUE_PAIRS_PER_BUFFER;
+constexpr uint32_t VALUES_PER_BUFFER = 2 * VALUE_PAIRS_PER_BUFFER;
 
-// Number of buffer bytes per a read packet.  
+// Number of buffer bytes per a read packet.
 // Increasing this value 'too much' causes in stability (with IDF 4.4.3).
-static constexpr uint32_t _BYTES_PER_BUFFER = _VALUES_PER_BUFFER * _BYTES_PER_VALUE;
-
+constexpr uint32_t BYTES_PER_BUFFER = VALUES_PER_BUFFER * BYTES_PER_VALUE;
 
 // Num of buffer in the ADC/DMA circular queue. We want to have
 // a sufficient size to allow backlog in processing.
-static constexpr uint32_t _NUM_BUFFERS = 2048 / _VALUE_PAIRS_PER_BUFFER;
+constexpr uint32_t NUM_BUFFERS = 2048 / VALUE_PAIRS_PER_BUFFER;
 
 #if !CONFIG_IDF_TARGET_ESP32
 #error "Unexpected target CPU."
 #endif
 
 static const adc_digi_init_config_t adc_dma_config = {
-    .max_store_buf_size = _NUM_BUFFERS * _BYTES_PER_BUFFER,
-    .conv_num_each_intr = _BYTES_PER_BUFFER,
+    .max_store_buf_size = NUM_BUFFERS * BYTES_PER_BUFFER,
+    .conv_num_each_intr = BYTES_PER_BUFFER,
     .adc1_chan_mask = BIT(6) | BIT(7),
     .adc2_chan_mask = 0,
 };
@@ -46,13 +45,13 @@ static const adc_digi_init_config_t adc_dma_config = {
 // of SOC_ADC_PATT_LEN_MAX?
 static adc_digi_pattern_config_t adc_pattern[] = {
     {
-        .atten = ADC_ATTEN_DB_0,
+        .atten = ADC_ATTEN_DB_11,
         .channel = 6,
         .unit = 0,  /// ADC_UNIT_1,
         .bit_width = SOC_ADC_DIGI_MAX_BITWIDTH,
     },
     {
-        .atten = ADC_ATTEN_DB_0,
+        .atten = ADC_ATTEN_DB_11,
         .channel = 7,
         .unit = 0,  // ADC_UNIT_1,
         .bit_width = SOC_ADC_DIGI_MAX_BITWIDTH,
@@ -74,10 +73,9 @@ static const adc_digi_configuration_t dig_cfg = {
     .format = ADC_DIGI_OUTPUT_FORMAT_TYPE1,
 };
 
-static uint8_t bytes_buffer[_BYTES_PER_BUFFER] = {0};
+static uint8_t bytes_buffer[BYTES_PER_BUFFER] = {0};
 
 void adc_task(void *ignored) {
-
   bool in_order = false;
   uint32_t order_changes = 0;
   uint32_t buffers_count = 0;
@@ -86,12 +84,12 @@ void adc_task(void *ignored) {
   for (;;) {
     io::LED1.clear();
     uint32_t num_ret_bytes = 0;
-    esp_err_t err_code = adc_digi_read_bytes(bytes_buffer, _BYTES_PER_BUFFER,
+    esp_err_t err_code = adc_digi_read_bytes(bytes_buffer, BYTES_PER_BUFFER,
                                              &num_ret_bytes, ADC_MAX_DELAY);
     io::LED1.set();
 
     // Sanity check the results.
-    if (err_code != ESP_OK || num_ret_bytes != _BYTES_PER_BUFFER) {
+    if (err_code != ESP_OK || num_ret_bytes != BYTES_PER_BUFFER) {
       printf("ADC read failed: %0x %u\n", err_code, num_ret_bytes);
       assert(false);
     }
@@ -118,23 +116,22 @@ void adc_task(void *ignored) {
 
     if (timer.elapsed_millis() >= 1000) {
       timer.reset();
-      printf("ADC: %s, %u, %u\n", (in_order? "(6,7)" : "(7,6)"), order_changes, buffers_count);
+      printf("ADC: %s, [%4u, %4u]  %u, %u\n", (in_order ? "[6,7]" : "[7,6]"),
+             values[0].type1.data, values[1].type1.data, order_changes,
+             buffers_count);
     }
 
     // printf("%hu %hu %4hu %4hu\n", values[0].type1.channel,
     //        values[1].type1.channel, values[0].type1.data,
     //        values[1].type1.data);
 
-    // uint16_t chan0 = values[0].type1.channel;
-    // uint16_t chan1 = values[1].type1.channel;
-
     // We expect the buffer to have the same order of pairs.
-    for (int i = 0; i < _VALUE_PAIRS_PER_BUFFER; i += 2) {
+    for (int i = 0; i < VALUE_PAIRS_PER_BUFFER; i += 2) {
       bool ok = (values[i].type1.channel == (in_order ? 6 : 7)) &&
                 (values[i + 1].type1.channel == (in_order ? 7 : 6));
       if (!ok) {
         printf("\n----\n\n");
-        for (int j = 0; j < _VALUE_PAIRS_PER_BUFFER; j += 2) {
+        for (int j = 0; j < VALUE_PAIRS_PER_BUFFER; j += 2) {
           printf("%3d: %hu %4hu %hu %4hu\n", j, values[j + 0].type1.channel,
                  values[j + 0].type1.data, values[j + 1].type1.channel,
                  values[j + 1].type1.data);
