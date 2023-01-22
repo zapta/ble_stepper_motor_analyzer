459def9a5e80b39997b3cbdb8b3e3f5b082362ff
Zapta <zapta@users.noreply.github.com>
2023-01-21 18:48:26 -0800

    Work in progress.

--- platformio/src/ble/ble_util.h
+++ platformio/src/ble/ble_util.h
@@ -1,12 +1,15 @@
 #pragma once
 
+#include "assert.h"
 #include "esp_gap_ble_api.h"
 #include "esp_gatts_api.h"
+#include "esp_log.h"
 
-// NOTE: UUID Macros are from 
+// NOTE: UUID Macros are from
 // https://docs.zephyrproject.org/apidoc/latest/uuid_8h_source.html
 
-// Encoded as 16 hex bytes, LSB first. 
+// Encoded as 16 hex bytes, LSB first.
+// clang-format off
 #define ENCODE_UUID_128(w32, w1, w2, w3, w48) \
         (((w48) >>  0) & 0xFF), \
         (((w48) >>  8) & 0xFF), \
@@ -23,14 +26,67 @@
         (((w32) >>  0) & 0xFF), \
         (((w32) >>  8) & 0xFF), \
         (((w32) >> 16) & 0xFF), \
-        (((w32) >> 24) & 0xFF) 
+        (((w32) >> 24) & 0xFF)
+// clang-format on
 
 // Encoded as 16 hex bytes, LSB first.
+// clang-format off
 #define ENCODE_UUID_16(w16)  \
         (((w16) >>  0) & 0xFF), \
         (((w16) >>  8) & 0xFF)
+// clang-format on
 
 namespace ble_util {
+
+class BigEndianEncoder {
+ public:
+  BigEndianEncoder(uint8_t* p_start, uint16_t size)
+      : _p_start(p_start), _p_end(p_start + size), _p_next(p_start){};
+
+  int capacity() { return _p_end - _p_start; }
+  int size() { return _p_next - _p_start; }
+
+  inline void encode_uint8(uint8_t v) {
+    check_avail(1);
+    *_p_next++ = v;
+  }
+
+  inline void encode_uint16(uint16_t v) {
+    check_avail(2);
+    *_p_next++ = v >> 8;
+    *_p_next++ = v >> 0;
+  }
+
+  inline void encode_uint24(uint32_t v) {
+    check_avail(3);
+    *_p_next++ = v >> 16;
+    *_p_next++ = v >> 8;
+    *_p_next++ = v >> 0;
+  }
+
+  inline void encode_uint32(uint32_t v) {
+    check_avail(4);
+    *_p_next++ = v >> 24;
+    *_p_next++ = v >> 16;
+    *_p_next++ = v >> 8;
+    *_p_next++ = v >> 0;
+  }
+
+ private:
+  const uint8_t* _p_start;
+  const uint8_t* _p_end;
+  uint8_t* _p_next;
+
+  static constexpr const char* ENCODER_TAG = "msb_enc";
+
+  inline void check_avail(uint16_t num_bytes) {
+    if ((_p_next + num_bytes) > _p_end) {
+      ESP_LOGE(ENCODER_TAG, "Insufficient space: requested %hu, available %d",
+               num_bytes, (_p_end - _p_next));
+      assert(false);
+    };
+  }
+};
 
 const char* gatts_event_name(esp_gatts_cb_event_t event);
 const char* gap_ble_event_name(esp_gap_ble_cb_event_t event);
