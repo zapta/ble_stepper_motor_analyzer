a91a04ca4a31269b9abbee0a8849aad36eb8228b
Zapta <zapta@users.noreply.github.com>
2023-01-27 20:52:11 -0800

    Minor cleanup

--- tools/flash.sh
+++ tools/flash.sh
@@ -2,7 +2,7 @@
 
 release="../platformio/.pio/build/release"
 
-./esptool.exe --chip esp32 write_flash  \
+./esptool.exe --chip esp32 write_flash \
     0x1000 ${release}/bootloader.bin \
     0x8000 ${release}/partitions.bin \
     0x10000 ${release}/firmware.bin
