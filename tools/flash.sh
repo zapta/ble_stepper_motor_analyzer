b47741a4ae5de9ed2f4343e3f44f1b53d6cb7d0f
Zapta <zapta@users.noreply.github.com>
2023-01-27 20:51:38 -0800

    Simplified esptool command

--- tools/flash.sh
+++ tools/flash.sh
@@ -2,10 +2,7 @@
 
 release="../platformio/.pio/build/release"
 
-./esptool.exe \
-  --chip esp32 --port "COM6" --baud 512200 --before default_reset --after hard_reset  \
-  write_flash  \
-    -z --flash_mode dio --flash_freq 40m --flash_size 4MB \
+./esptool.exe --chip esp32 write_flash  \
     0x1000 ${release}/bootloader.bin \
     0x8000 ${release}/partitions.bin \
     0x10000 ${release}/firmware.bin
