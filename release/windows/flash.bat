1459075866d7c1acb96ca5068b4b9ea477b77f96
Zapta <zapta@users.noreply.github.com>
2023-01-28 17:55:12 -0800

    Updated windows flasher.

---
+++ release/windows/flash.bat
@@ -0,0 +1,7 @@
+:: A batch file to flash an esp32 analyzer device.
+
+esptool.exe --chip esp32 write_flash ^
+  0x1000  ..\firmware\bootloader.bin ^
+  0x8000  ..\firmware\partitions.bin ^
+  0x10000 ..\firmware\firmware.bin
+
--- tools/flash.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-release="../platformio/.pio/build/release"
-
-./esptool.exe --chip esp32 write_flash \
-    0x1000 ${release}/bootloader.bin \
-    0x8000 ${release}/partitions.bin \
-    0x10000 ${release}/firmware.bin
-
