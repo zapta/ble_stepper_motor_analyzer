e2a001021e11357872c0211107b457fc4e69f7cb
Zapta <zapta@users.noreply.github.com>
2023-01-30 21:14:14 -0800

    Minor tweaks.

--- analyzer/test/scanner.py
+++ analyzer/test/scanner.py
@@ -5,7 +5,8 @@ from bleak import BleakScanner
 
 async def scan():
     print("Scanning for 5 secs ...", flush=True)
-    devices = await BleakScanner.discover(timeout=5)
+    scanner = BleakScanner()
+    devices = await scanner.discover(timeout=5)
     for device in devices:
         print(device, flush=True)
         #name = device.name or ""
