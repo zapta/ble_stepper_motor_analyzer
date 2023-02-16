6a964ffc5de10ff119976c6129c5f3cb985d75b3
Zapta <zapta@users.noreply.github.com>
2023-02-16 13:07:09 -0800

    Updated python test programs.

---
+++ app/test/connection_close.py
@@ -0,0 +1,39 @@
+# A test program for a bleak connection that is explicitly closed.
+
+import asyncio
+import platform
+import sys
+import atexit
+import time
+import signal
+from bleak import BleakClient, BleakScanner
+import signal
+
+# Adapt to your actual device.
+device_address = "0C:8B:95:F2:B4:36"
+
+signal.signal(signal.SIGINT, lambda number, frame: sys.exit())
+
+print(f"OS: {platform.platform()}", flush=True)
+print(f"Platform:: {platform.uname()}", flush=True)
+print(f"Python {sys.version}", flush=True)
+
+async def test():
+    #global client
+    print(f"Trying to connect to {device_address}", flush=True)
+    device = await BleakScanner.find_device_by_address(device_address, timeout=10.0)
+    assert device
+    client = BleakClient(device)
+    assert client
+    await client.connect(timeout=10.0)
+    assert client.is_connected
+    print(f"Connected", flush=True)
+    print(f"Waiting 5 secs...", flush=True)
+    time.sleep(5.0)
+    print(f"Closing...", flush=True)
+    assert client.is_connected
+    await client.disconnect()
+    print(f"Test done", flush=True)
+
+event_loop = asyncio.new_event_loop()
+event_loop.run_until_complete(test())
