2d32e2d76fc9a5499a8f12fc83c1034aaca317c3
Zapta <zapta@users.noreply.github.com>
2023-10-08 10:50:10 -0700

    Updated windows build file for latest python and pyqtgraph

--- python/pyinstaller/build_on_windows.bat
+++ python/pyinstaller/build_on_windows.bat
@@ -1,5 +1,9 @@
 :: A Windows batch file to build a single file executable of the 
 :: analyzer's app.
+::
+:: See this bug regarding an error in pyqtgraph and the workaround of swapping
+:: two lines:
+:: https://github.com/pyinstaller/pyinstaller/issues/7991#issuecomment-1752032919
 
 ::set -e
 ::set -o xtrace
@@ -16,6 +20,7 @@ mkdir _build
 mkdir _spec
 
 pyinstaller ..\analyzer/analyzer.py ^
+  --hidden-import winrt.windows.foundation.collections ^
   --paths ".." ^
   --clean ^
   --onefile ^
